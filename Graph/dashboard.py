a366bcb37fb24a8aeaeb292c28f59da8a7ef96ad
JoanMz <joanmzteo@gmail.com>
2024-11-17 01:25:19 -0500

    finish

--- Graph/dashboard.py
+++ Graph/dashboard.py
@@ -4,8 +4,9 @@ import Pysqlconnect
 import json
 import joblib
 import pandas as pd
-from kafka import KafkaConsumer
 import streamlit as st
+from kafka import KafkaConsumer
+import altair as alt
 
 
 # Charge my fit model
@@ -25,19 +26,23 @@ framework = pd.DataFrame(columns=['GDP', 'Family', 'Life Expectancy', 'Freedom',
           'Sub-Saharan Africa', 'Southern Asia', 'Score', 'Predicted_Score'])
 
 results_df = framework.copy()
+paint_df = framework.copy()
 # read the  data, predict and finally load the data at postgres 
 coh=0
 count=0
+
+st.title('Real Time Dashboard')
+chart_placeholder = st.empty()
+
 for message in consumer:
     data = message.value
-    X_new = pd.DataFrame([data], columns=['GDP', 'Family', 'Life Expectancy', 'Freedom', 'Trust', '2015', '2016', '2017', '2018', '2019',
+    X_new = pd.DataFrame([data],columns=['GDP', 'Family', 'Life Expectancy', 'Freedom', 'Trust', '2015', '2016', '2017', '2018', '2019',
           'Western Europe', 'North America', 'Australia and New Zealand', 'Middle East and Northern Africa',
           'Latin America and Caribbean', 'Southeastern Asia', 'Central and Eastern Europe', 'Eastern Asia',
-          'Sub-Saharan Africa', 'Southern Asia'])
+          'Sub-Saharan Africa', 'Southern Asia'])  #Caracteristic list
     
-    # Delete the rows with empty values
+    # Del rows with null values
     if X_new.isnull().values.any():
-        #print(f"Data with NaN values: {X_new}")
         print(f"{coh} errS")
         coh +=1
         continue
@@ -45,6 +50,7 @@ for message in consumer:
     predicted_score = model.predict(X_new)[0]
     data['Predicted_Score'] = predicted_score
     results_df = pd.concat([results_df, pd.DataFrame([data])], ignore_index=True)
+    paint_df = pd.concat([paint_df, pd.DataFrame([data])], ignore_index=True)
 
     y_true = results_df['Score']
     y_pred = results_df['Predicted_Score']
@@ -53,10 +59,27 @@ for message in consumer:
     print(f"Value to predict : {y_true.iloc[shap]}   Predict value: {y_pred.iloc[shap]}")
     count+=1
 
+    # Prepair the data for Altair
+    chart_data = paint_df.reset_index().melt('index', value_vars=['Score', 'Predicted_Score'], var_name='Variable', value_name='Value')
 
-
+    #Set the colors of graph
+    color_scale = alt.Scale(
+        domain=['Score', 'Predicted_Score'],
+        range=['#98ffe0', '#b92a3e']
+    )
 
     
+    chart = alt.Chart(chart_data).mark_line().encode(
+        x='index:Q',
+        y='Value:Q',
+        color=alt.Color('Variable:N', scale=color_scale)
+    ).properties(
+        width=700,
+        height=400
+    )
+
+    
+    chart_placeholder.altair_chart(chart, use_container_width=True)
 
     if count >= 20:
         try:
@@ -66,3 +89,4 @@ for message in consumer:
             results_df = framework.copy()
         except:
             print("Something wrong happens")
+
--- Kafka/app.py
+++ Kafka/app.py
@@ -1,5 +1,13 @@
+import threading
 import kafka_producer, kafka_consumer
 
 if __name__ == "__main__":
-    kafka_producer()
-    kafka_consumer()
+    t1 = threading.Thread(kafka_consumer())
+    t2 = threading.Thread(kafka_producer())
+
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
